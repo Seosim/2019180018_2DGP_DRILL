4e60edb990af1a1c1b6a92a4773f92753dd54524
Seosim <103580344+Seosim@users.noreply.github.com>
2022-09-22 17:27:59 +0900

    DrawImage 함수 생성

--- 07/anim_viewer.py
+++ 07/anim_viewer.py
@@ -4,32 +4,29 @@ open_canvas()
 
 character = load_image("sprite.png")
 
-def DrawImage():
-    pass
+def DrawImage(x,y,fr,ac):
+    clear_canvas()
+    character.clip_draw(fr * 86 + 2, ac * 95, 86, 90, x, y)
+    update_canvas()
+    fr = (fr+1)%10
+    delay(0.05)
+    get_events()
 
 def Right():
     frame = 0
     action = 0
     for x in range(50,750+1,10):
-        clear_canvas()
-        character.clip_draw(frame * 86+2, action*90, 86, 90, x, 90)
-        update_canvas()
-        frame = (frame + 1) % 10
-        delay(0.05)
-        get_events()
+        DrawImage(x,90,frame,0)
+        frame = (frame+1)%10
 
 def Left():
     frame = 0
     action = 2
     for x in range(750,50-1,-10):
-        clear_canvas()
-        character.clip_draw(frame * 86+2, action*95, 86, 90, x, 90)
-        update_canvas()
-        frame = (frame + 1) % 10
-        delay(0.05)
-        get_events()
+        DrawImage(x,90,frame,2)
+        frame = (frame+1)%10
 
 
 
-#Right()
+Right()
 Left()
