c6eb642039908d7440fdfad05ef5b0f168841f40
Seosim <103580344+Seosim@users.noreply.github.com>
2022-09-22 17:15:41 +0900

    우로움직임 함수 생성

--- 07/anim_viewer.py
+++ 07/anim_viewer.py
@@ -1,6 +1,23 @@
 from pico2d import *
 
-def move(x,y):
-
-
 open_canvas()
+
+character = load_image("sprite.png")
+
+def DrawImage():
+    pass
+
+def Right():
+    frame = 0
+    action = 0
+    for x in range(50,750,10):
+        clear_canvas()
+        character.clip_draw(frame * 86+1, 0, 86, 90, x, 300)  # x1 y1 x2 y2
+        update_canvas()
+        frame = (frame + 1) % 10
+        delay(0.05)
+        get_events()
+
+
+
+Right()
