2b93f7914ccae7a5e434eedfe505b6c764d3855e
Seosim <103580344+Seosim@users.noreply.github.com>
2022-09-22 17:20:28 +0900

    좌로움직임 함수생성/우로움직임 수정

--- 07/anim_viewer.py
+++ 07/anim_viewer.py
@@ -10,9 +10,20 @@ def DrawImage():
 def Right():
     frame = 0
     action = 0
-    for x in range(50,750,10):
+    for x in range(50,750+1,10):
         clear_canvas()
-        character.clip_draw(frame * 86+1, 0, 86, 90, x, 300)  # x1 y1 x2 y2
+        character.clip_draw(frame * 86+2, action*90, 86, 90, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 10
+        delay(0.05)
+        get_events()
+
+def Left():
+    frame = 0
+    action = 2
+    for x in range(750,50-1,-10):
+        clear_canvas()
+        character.clip_draw(frame * 86+2, action*95, 86, 90, x, 90)
         update_canvas()
         frame = (frame + 1) % 10
         delay(0.05)
@@ -20,4 +31,5 @@ def Right():
 
 
 
-Right()
+#Right()
+Left()
